13ebe3ef7c5d84fd839a3e77a99fc048785dd6f9
DanielCardozoH13 <cardozodaniel36@gmail.com>
2019-06-05 00:18:34 -0500

    v1

--- prueba_graficas.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot
-
-lenguajes = ('python', 'c', 'java')
-slices = (100, 130, 90)
-colores = ('red', 'blue', 'green')
-
-pyplot.pie(slices, colors=colores)
-#pyplot.show()
-pyplot.savefig('prueba.png')
